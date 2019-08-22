861c4ff4bc5076c31a9b92908c74a263b0b78475
arma7x <arma7x@live.com>
2019-08-23 00:10:54 +0800

    WIP Chat

--- functions/functions/src/index.ts
+++ functions/functions/src/index.ts
@@ -139,6 +139,98 @@ export const joinQueue = functions.https.onRequest((request, response) => {
   });
 });
 
+export const deleteQueue = functions.https.onRequest((request, response) => {
+  const corsFn = cors();
+  const token = request.query.token || 'string';
+  admin.auth().verifyIdToken(token)
+  .then((decodedToken) => {
+    if (request.query.topic === undefined || request.query.topic === null) {
+      return Promise.reject({ 'message': 'Please enter topic of discussion' });
+    }
+    const chatRef = admin.database().ref('/chats/'+decodedToken.uid);
+    return chatRef.once("value")
+    .then((snapshot) => {
+      if (snapshot.exists()){
+        return Promise.reject({ 'message': 'Already in queue' });
+      } else {
+        return admin.database().ref('/queues/'+decodedToken.uid).set({
+          "topic": request.query.topic,
+          "timestamp": new Date().getTime(),
+          "status": 0,
+          "assigned_user": false
+        })
+        .then(() => {
+          return chatRef.set({
+            "topic": request.query.topic,
+            "timestamp": new Date().getTime(),
+            "status": 0,
+            "assigned_user": false
+          });
+        })
+        .catch((error) => {
+          return error;
+        });
+      }
+    });
+  })
+  .then(() => {
+    corsFn(request, response, () => {
+      response.status(200).json({ 'message': 'Your have joined queued' });
+    });
+  })
+  .catch((error) => {
+    corsFn(request, response, () => {
+      response.status(400).json(error);
+    });
+  });
+});
+
+export const adminDeleteQueue = functions.https.onRequest((request, response) => {
+  const corsFn = cors();
+  const token = request.query.token || 'string';
+  admin.auth().verifyIdToken(token)
+  .then((decodedToken) => {
+    if (request.query.topic === undefined || request.query.topic === null) {
+      return Promise.reject({ 'message': 'Please enter topic of discussion' });
+    }
+    const chatRef = admin.database().ref('/chats/'+decodedToken.uid);
+    return chatRef.once("value")
+    .then((snapshot) => {
+      if (snapshot.exists()){
+        return Promise.reject({ 'message': 'Already in queue' });
+      } else {
+        return admin.database().ref('/queues/'+decodedToken.uid).set({
+          "topic": request.query.topic,
+          "timestamp": new Date().getTime(),
+          "status": 0,
+          "assigned_user": false
+        })
+        .then(() => {
+          return chatRef.set({
+            "topic": request.query.topic,
+            "timestamp": new Date().getTime(),
+            "status": 0,
+            "assigned_user": false
+          });
+        })
+        .catch((error) => {
+          return error;
+        });
+      }
+    });
+  })
+  .then(() => {
+    corsFn(request, response, () => {
+      response.status(200).json({ 'message': 'Your have joined queued' });
+    });
+  })
+  .catch((error) => {
+    corsFn(request, response, () => {
+      response.status(400).json(error);
+    });
+  });
+});
+
 exports.onUserCreated = functions.auth.user().onCreate((user) => {
   admin.database().ref('/users/'+user.uid).set({'role': LEVEL.MEMBER})
   .then(() => {
